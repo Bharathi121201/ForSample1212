1455e04d4eeb0c0db91e120bdae4b8b554fa8066
Bharathi121201 <bharathivja12@gmail.com>
2022-07-10 22:52:19 +0530

    Pavi Modified the codes

--- src/test/sample/GitSam.java
+++ src/test/sample/GitSam.java
@@ -1,5 +1,23 @@
 package test.sample;
 
 public class GitSam {
+	
+	public void sing() {
+		System.out.println("Singing");
 
+	}
+public static void main(String[] args) {
+	GitSam gs=new GitSam();
+	gs.sing();
+	
+}
+
+    private void laugh() {
+		System.out.println("Laughing");
+
+	}
+    private void love() {
+		System.out.println("Loving");
+
+	}
 }
